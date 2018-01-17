7f87a6ce315cf153542d1e6ac17fc04e70de98a2
hadihariri <mail@hadihariri.com>
2018-01-17 12:19:10 +0100

    Added App Engine demo

---
+++ google-appengine-sample/src/main/kotlin/com/jetbrains/demo/MessageController.kt
@@ -0,0 +1,14 @@
+package com.jetbrains.demo
+
+import org.springframework.web.bind.annotation.*
+
+
+data class Message(val text: String, val priority: String)
+
+@RestController
+class MessageController {
+    @RequestMapping("/message")
+    fun message(): Message {
+        return Message("Hello from Google App Engine", "High")
+    }
+}
