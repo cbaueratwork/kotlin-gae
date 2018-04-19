501c10930e631bfa0d8d1dc41f29e1ccf44c46e3
Chris Bauer <bauerpower@google.com>
2018-04-19 11:37:17 -0700

    adding subclasses and companion objects

--- google-compute-sample/instance-startup.sh
+++ google-compute-sample/instance-startup.sh
@@ -17,6 +17,14 @@ apt-get -y --force-yes install openjdk-8-jdk
 # Make Java 8 default
 update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
 
+# Install custom cdbg instance
+git clone https://github.com/cbaueratwork/cloud-debug-java
+pushd cloud-debug-java
+chmod +x build.sh
+./build.sh
+mkdir -p /opt/cdbg
+tar -xvzC /opt/cdbg -f cdbg_java_agent_gce.tar.gz
+
 # Start server
 java -agentpath:/opt/cdbg/cdbg_java_agent.so -Dcom.google.cdbg.module="Kotlin Demo" -Dcom.google.cdbg.version="1.0" -jar demo.jar
 
--- google-compute-sample/src/main/kotlin/com/jetbrains/demo/MessageController.kt
+++ google-compute-sample/src/main/kotlin/com/jetbrains/demo/MessageController.kt
@@ -3,11 +3,16 @@ package com.jetbrains.demo
 import org.springframework.web.bind.annotation.*
 
 
+interface IMessage {
+  val text: String
+  val priority: String
+}
+
 data class Message(
-        val text: String,
-        val priority: String,
-        val widget: Array<Widget>
-)
+  override val text: String,
+  override val priority: String,
+  val widget: Array<Widget>
+) : IMessage
 
 object Blurb {
     fun getBlurb(fromData: Boolean): String {
@@ -17,33 +22,100 @@ object Blurb {
     val blurbData = "flerber"
 }
 
-private fun generateBlurb(): Widget{
+private fun generateBlurb(): Widget {
     return Widget("generated")
 }
 
 @RestController
 class MessageController {
-    @RequestMapping("/message")
-    fun message(): Message {
-        val customWidget = Widget("trinket")
-        val defaultWidget = Widget()
-        val defaultBlurbWidget = Widget(Blurb.getBlurb(true))
-        val blurbWidget = Widget(Blurb.getBlurb(false))
-        var generatedWidget = Widget()
-        generatedWidget = generateBlurb()
+  @RequestMapping("/message")
+  fun message(): Message {
+    val customWidget = Widget("trinket")
+    val defaultWidget = Widget()
+    val defaultBlurbWidget = Widget(Blurb.getBlurb(true))
+    val blurbWidget = Widget(Blurb.getBlurb(false))
+    var generatedWidget = Widget()
+    generatedWidget = generateBlurb()
 
-        return Message(
-                "Hello from Cloud Debugger",
-                "High",
-                arrayOf(
-                        defaultWidget,
-                        customWidget,
-                        defaultBlurbWidget,
-                        blurbWidget,
-                        generatedWidget
-                )
+    return Message(
+        "Hello from Cloud Debugger",
+        "High",
+        arrayOf(
+            defaultWidget,
+            customWidget,
+            defaultBlurbWidget,
+            blurbWidget,
+            generatedWidget
         )
+    )
+  }
+
+  @RequestMapping("/submessage")
+  fun subMessage(): SubMessage {
+    val customSubWidget = SubWidget("trinket")
+    val defaultSubWidget = SubWidget()
+    val defaultBlurbSubWidget = SubWidget(Blurb.getBlurb(true))
+    val blurbSubWidget = SubWidget(Blurb.getBlurb(false))
+    var generatedSubWidget = SubWidget()
+    generatedSubWidget = MessageController.generateSubBlurb()
+
+    return SubMessage(
+        "Hello from Cloud Debugger",
+        "High",
+        "Surprise!",
+        arrayOf(
+            defaultSubWidget,
+            customSubWidget,
+            defaultBlurbSubWidget,
+            blurbSubWidget,
+            generatedSubWidget
+        )
+    )
+  }
+
+  companion object {
+    fun generateSubBlurb(): SubWidget {
+      return SubWidget("subgenerated")
     }
+  }
+
+  object SubObject {
+    fun getSubObject(fromData: Boolean): String {
+      return if (fromData) this.subData else "subobject"
+    }
+
+    val subData = "burber"
+  }
+
+  data class SubMessage(
+      override val text: String,
+      override val priority: String,
+      val newProp: String = "Test",
+      val widget: Array<SubWidget>
+  ): IMessage
+
+  // Primary constructor needs a 'type' parameter.
+  class SubWidget(val type: String) {
+    val assignedType: String;
+
+    // Declare a field.
+    val classifiedAs = "Widget"
+
+    // Use the parameter outside of any blocks.
+    val allCaps = type.toUpperCase()
+
+    init {
+      // Use the parameter in an init block.
+      this.assignedType = type
+    }
+
+    // Create a secondary constructor which delegates to the primary
+    // constructor.
+    constructor() : this("default") {
+      // init blocks are executed before secondary constructor body.
+      val anotherAssignment = this.assignedType
+    }
+  }
 }
 
 // Primary constructor needs a 'type' parameter.
@@ -63,7 +135,7 @@ class Widget(val type: String) {
 
     // Create a secondary constructor which delegates to the primary
     // constructor.
-    constructor(): this("default") {
+    constructor() : this("default") {
         // init blocks are executed before secondary constructor body.
         val anotherAssignment = this.assignedType
     }
